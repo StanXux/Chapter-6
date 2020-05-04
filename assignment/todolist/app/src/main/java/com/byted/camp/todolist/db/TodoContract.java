f0c89bfd0a9048ad0ea889406f4126d2a305c140
StanXux <49066657+StanXux@users.noreply.github.com>
2020-05-04 20:47:32 +0800

    Add files in Dir db

--- assignment/todolist/app/src/main/java/com/byted/camp/todolist/db/TodoContract.java
+++ assignment/todolist/app/src/main/java/com/byted/camp/todolist/db/TodoContract.java
@@ -1,5 +1,7 @@
 package com.byted.camp.todolist.db;
 
+import android.provider.BaseColumns;
+
 /**
  * Created on 2019/1/22.
  *
@@ -9,7 +11,25 @@ public final class TodoContract {
 
     // TODO 定义表结构和 SQL 语句常量
 
+    public static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TodoEntry.TABLE_NAME + "(" +
+                    TodoEntry._ID + " INTEGER PRIMARY KEY, " +
+                    TodoEntry.COLUMN_DATE + " TEXT, " +
+                    TodoEntry.COLUMN_STATE + " TEXT, " +
+                    TodoEntry.COLUMN_CONTENT + " TEXT, " +
+                    TodoEntry.COLUMN_LEVEL + " TEXT)";
+
+    public static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TodoEntry.TABLE_NAME;
+
     private TodoContract() {
     }
 
+    public static class TodoEntry implements BaseColumns{
+        public static final String TABLE_NAME = "note";
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_STATE = "state";
+        public static final String COLUMN_CONTENT = "content";
+        public static final String COLUMN_LEVEL = "level";
+    }
 }
--- assignment/todolist/app/src/main/java/com/byted/camp/todolist/db/TodoDbHelper.java
+++ assignment/todolist/app/src/main/java/com/byted/camp/todolist/db/TodoDbHelper.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.byted.camp.todolist.db.TodoContract.SQL_CREATE_ENTRIES;
+import static com.byted.camp.todolist.db.TodoContract.SQL_DELETE_ENTRIES;
+
 /**
  * Created on 2019/1/22.
  *
@@ -12,19 +15,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class TodoDbHelper extends SQLiteOpenHelper {
 
     // TODO 定义数据库名、版本；创建数据库
+    public static final int DATABASE_VERSION = 2;
+    public static final String DATABASE_NAME = "TodoList.db";
 
     public TodoDbHelper(Context context) {
-        super(context, "todo", null, 0);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        if(1 == oldVersion && 2 == newVersion)
+        {
+            String str_v2 = "ALTER TABLE " + TodoContract.TodoEntry.TABLE_NAME + " ADD " +
+                    TodoContract.TodoEntry.COLUMN_LEVEL + " DEFAULT 1";
+            db.execSQL(str_v2);
+        }
     }
 
 }
